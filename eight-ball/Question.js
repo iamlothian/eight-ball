92d1b10080ea567f1f3a3476e263070a181bc992
Matthew Lothian <matthew.lothian@iit-laptop.industrieit.local>
2015-06-13 15:29:55 +1000

    stable

--- eight-ball/Question.js
+++ eight-ball/Question.js
@@ -1,23 +1,36 @@
-/**
-
-**/
-Question = function Question () {
-  // body...
-}
-
 
 Question = (function(){
-  
-  /* collection */
-  questions = new Mongo.Collection("questions");
 
-  /* constructor*/
-  var constructor = function Question () {
+	/* collection */
+	var questions = new Mongo.Collection("questions");
 
+	/* constructor*/
+	var constructor = function Question () {
 
+		this.text = "";
+		this.timeout = moment();
+		this.conversationId;
 
-  }
+	};
 
-  return constructor;
+	constructor.addQuestion = function(text, momentOffset, callback) {
+
+		var q = new Question();
+
+		questions.insert(q, function(error, id){
+
+			!!error ? 
+				console.log("INSETED FAILED", error) :
+				console.log("INSETED QUESTION: ", id);
+
+			if (!!id){
+				callback(id);
+			}
+
+		});
+
+	};
+
+	return constructor;
 
 })()
